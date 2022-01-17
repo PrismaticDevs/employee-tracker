f465c47895eeb758005b8ace20c7d8d93caddae0
Matthew Brignola <matthewbrignola@ymail.com>
2022-01-16 19:30:34 -0700

    able to add employees, now to update roles

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ const inquirer = require('inquirer');
 const connection = require('./connection');
 const uuid = require('./helpers/uuid');
 console.log(`
- ______________________________________________________________________________________
-|                           Welcome to                                                | |                ________  _                   _                                      | 
-|               |  ______|| |_________  __ __ | | ___   _    _  ___  ___              |
-|               |   _|    |  __   __  ||  '_ \| |/ _ \ | |  | |/ _ \/ _ \             |
-|               |  |_____ | |  |_|  | ||  |_||| | (_) || |__| |  __/  __/             |
-|               |________||_|       |_|| '___/|_|\___/  \__   |\___/\___/             |
-|                                      |_|             |_____/                        | 
-|
+ ____________________________________________________________________________________
+|                Welcome to                                                                                                                               |                ________  _                   _                                                                                                        
+|               |  ______|| |_________  __ __ | | ___   _    _  ___  ___                                                                               
+|               |   _|    |  __   __  ||  '_ \| |/ _ \ | |  | |/ _ \/ _ \                                                                                
+|               |  |_____ | |  |_|  | ||  |_||| | (_) || |__| |  __/  __/                                                                                
+|               |________||_|       |_|| '___/|_|\___/  \__   |\___/\___/                                                                                
+|                                      |_|             |_____/                                                                                            
+|                                                                                                                                                          |
 |
 |                                                                 
 |_____________________________________________________________________________________
 Brought to you by PrismaticDevs 
 `);
+// Options to choose from 
 const questions = () => inquirer.prompt([{
         type: "list",
         name: "todo",
@@ -26,22 +27,54 @@ const questions = () => inquirer.prompt([{
             "View all rolls",
             "Add role",
             "View all departments",
-            "Add deparment",
+            "Add department",
             "Exit"
         ]
     }, ])
     .then(answers => {
+        // Functions in reponse to prompt
         if (answers.todo === "Exit") {
             return console.log('To exit type Ctrl + C')
         };
+        // view all employees
         if (answers.todo === "View all employees") {
-            connection.query(
-                'SELECT * FROM employee',
-                function(err, results) {
-                    console.log(results);
-                }
-            );
-            questions();
-        };
+            try {
+                connection.query('SELECT * FROM employee', (err, results) => {
+                    console.log("All employees");
+                    for (let i = 0; i < results.length; i++) {
+                        console.log("_____________");
+                        console.log('Id:', results[i].id);
+                        console.log('First name:', results[i].first_name);
+                        console.log('Last name:', results[i].last_name);
+                        console.log("_____________");
+                    }
+                    questions();
+                });
+            } catch (error) {
+                console.error('Failed to fetch Data');
+            }
+        }; // END View all employees
+        // Add employee
+        if (answers.todo === "Add employee") {
+            inquirer.prompt([{
+                    type: "input",
+                    name: "first_name",
+                    message: "First name?",
+                }, ])
+                .then(answers => {
+                    let first_name = answers.first_name;
+                    try {
+                        connection.query(`INSERT INTO employee (id, first_name) VALUES ("${uuid()}","${first_name}");`, (err, results) => {
+                            if (err) throw err;
+                            questions();
+                        });
+                        console.log(`Inserted ${first_name}`);
+                    } catch (error) {
+                        console.error('Failed to fetch Data');
+                    }
+                });
+        }
+
     });
+// Initial evocation to run the prompts
 questions();
